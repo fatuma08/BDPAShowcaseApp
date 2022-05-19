d1b611a4b2a68d50c672ff15a8f3c4aad654ec4c
fatuma08 <86160816+fatuma08@users.noreply.github.com>
2022-05-18 21:46:39 -0500

    Updates Layouts

--- app/src/main/java/com/bdpa/citypath/Favorites.java
+++ app/src/main/java/com/bdpa/citypath/Favorites.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UpdateUser extends AppCompatActivity {
+public class Favorites extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_user);
+        setContentView(R.layout.activity_favorites);
     }
 }
---
+++ app/src/main/java/com/bdpa/citypath/ui/testItem.java
@@ -0,0 +1,23 @@
+package com.bdpa.citypath.ui;
+
+public class testItem {
+    private String itemName;
+    private String itemDetails;
+
+    public String getItemDetails() {
+        return itemDetails;
+    }
+
+    public void setItemDetails(String itemDetails) {
+        this.itemDetails = itemDetails;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+}
